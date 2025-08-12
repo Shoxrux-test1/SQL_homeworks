60fe02396be1c11827d41afee20e5f61f33374a6
Shoxrux-test1 <655shoxrux5353@gmail.com>
2025-08-12 11:17:48 +0500

    Update hometask01.sql

--- lesson-1/homework/hometask01.sql
+++ lesson-1/homework/hometask01.sql
@@ -1,7 +1,6 @@
-select * from students
-create database READBOOKSDB
+select * from books
 
 Create table books (book_id int, name varchar(30), author_name varchar(30),
 genre varchar(20), status varchar(30))
 
-select * from books
+
